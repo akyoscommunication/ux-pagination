d91e6d49424a03a001f96a713ee6c4ddfdc210a6
Johan JOULKVA <johan.akyos@gmail.com>
2024-10-25 17:38:35 +0200

    Fix css and namespace

--- UXPagination.php
+++ UXPagination.php
@@ -2,22 +2,13 @@
 
 namespace Akyos\UXPagination;
 
-use Akyos\UXPagination\DependencyInjection\UXPaginationExtention;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
 class UXPagination extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
-    }
-
-    public function getContainerExtension(): ?ExtensionInterface
-    {
-        if (null === $this->extension) {
-            $this->extension = new UXPaginationExtention();
-        }
-        return $this->extension;
     }
 }
